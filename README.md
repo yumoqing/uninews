cc759101d2105e9015fdbcff036e7eabf3cae498
yumoqing <yumoqing@gmail.com>
2022-08-22 21:04:47 +0800

    bugfix

--- README.md
+++ README.md
@@ -1,2 +1,82 @@
 # uninews
-a unique news feed api for all news provider
+a universal news feed api for all news provider
+
+## Installation
+```
+pip install uninews
+```
+
+## Supported providers
+
+* [TheNewsApp](https://TheNewsApi.com)
+* [NewsCatcherApi](https://newscatcherapi.com)
+* [NewsData.io](httsp://newsdata.io)
+* [GoogleNews](https://pypi.org/project/pygooglenews)
+
+## Usage
+```
+from uninews import NewsFeed
+from TheNewsApi import set_app_info
+
+set_app_info(appkey=mykey)
+nf = NewsFeet('TheNewsApp')
+x = nf.news()
+print(x)
+x = nf.topstorys()
+print(x)
+x = nf.headline()
+print(x)
+x = nf.topic('business')
+print(x)
+```
+
+## Features
+
+NewsFeed provide four type news get methods:
+news:
+	get news from the specified provider
+
+topic:
+	get topiced news from the specified provider
+
+headline:
+	get headline news from the specified provider
+
+topstory:
+	get top story news from the specified provider
+
+if the method do not provide the method, it will return None
+
+## Return data structure
+
+All the data return by those four method is the same, the top structure
+are a dict type with following key:
+1. total
+	total record returned
+2. page
+	current page of the data
+3. articles
+	a array contain all the articles
+
+article is a dict contains following keys:
+1. title
+	the article's title
+2. description
+	the summary description
+3. content
+	the article body
+4. link
+	the url address to the article
+5. img_link
+	the article image url address
+6. video_link
+	the video url address of article
+7. publish_date
+	the article publish date
+
+## provider
+
+it must implments uninews four same name method
+
+
+
--- uninews/newsfeed.py
+++ uninews/newsfeed.py
@@ -1,3 +1,8 @@
+import importlib
+
+def buildProvider(newsfeed, providerName):
+	_module = importlib.import_module(providerName)
+	return _module.buildProvider(tts)
 
 class NewsFeed:
 	def __init__(self, providerName):
@@ -6,6 +11,116 @@ class NewsFeed:
 			'total',
 			'articles'
 			'page'
+		]
+		self.catepory = [
+            'business',
+            'entertainment',
+            'environment',
+            'food',
+            'health',
+            'politics',
+            'science',
+            'sports',
+            'technology',
+            'top',
+            'world'
+		]
+		self.languages = [
+			'ar',
+			'bg',
+			'bn',
+			'cs',
+			'da',
+			'de',
+			'el',
+			'en',
+			'es',
+			'et',
+			'fa',
+			'fi',
+			'fr',
+			'he',
+			'hi',
+			'hr',
+			'hu',
+			'id',
+			'it',
+			'ja',
+			'ko',
+			'lt',
+			'multi',
+			'nl',
+			'no',
+			'pl',
+			'pt',
+			'ro',
+			'ru',
+			'sk',
+			'sl',
+			'sv',
+			'ta',
+			'th',
+			'tr',
+			'uk',
+			'vi',
+			'zh'
+		]
+		self.countries = [
+			'ar',
+			'am',
+			'au',
+			'at',
+			'by',
+			'be',
+			'bo',
+			'br',
+			'bg',
+			'ca',
+			'cl',
+			'cn',
+			'co',
+			'hr',
+			'cz',
+			'ec',
+			'eg',
+			'fr',
+			'de',
+			'gr',
+			'hn',
+			'hk',
+			'in',
+			'id',
+			'ir',
+			'ie',
+			'il',
+			'it',
+			'jp',
+			'kr',
+			'mx',
+			'nl',
+			'nz',
+			'ni',
+			'pk',
+			'pa',
+			'pe',
+			'pl',
+			'pt',
+			'qa',
+			'ro',
+			'ru',
+			'sa',
+			'za',
+			'es',
+			'ch',
+			'sy',
+			'tw',
+			'th',
+			'tr',
+			'ua',
+			'gb',
+			'us',
+			'uy',
+			've'
 		]
 		self.article_keys = [
 			'title',
@@ -101,8 +216,17 @@ class NewsFeed:
 			data['articles'] = articles
 		return data
 
+	def get_data_by_key(self, data, k_str):
+		ks = '.'.split()
+		x = data.get(ks[0]
+		for k in ks[1:]:
+			if x is None:
+				return None
+			x = x.get(k)
+		return x
+
 	def dictmap(self, keys, mapping, data):
-		x = {k:data.get(mapping.get(k,' '), None) for k in keys}
+		x = {k:self.get_data_by_key(data, mapping.get(k,k)) for k in keys}
 		return x
 
 	def array2param(self, arr, delimiter=','):
@@ -110,3 +234,62 @@ class NewsFeed:
 		if s == '':
 			return None
 		return s
+
+	def get_countrys(self):
+		return {
+				'ar':'Argentina',
+				'am':'Armenia',
+				'au':'Australia',
+				'at':'Austria',
+				'by':'Belarus',
+				'be':'Belgium',
+				'bo':'Bolivia',
+				'br':'Brazil',
+				'bg':'Bulgaria',
+				'ca':'Canada',
+				'cl':'Chile',
+				'cn':'China',
+				'co':'Colombia',
+				'hr':'Croatia',
+				'cz':'Czechia',
+				'ec':'Ecuador',
+				'eg':'Egypt',
+				'fr':'France',
+				'de':'Germany',
+				'gr':'Greece',
+				'hn':'Honduras',
+				'hk':'Hong Kong',
+				'in':'India',
+				'id':'Indonesia',
+				'ir':'Iran',
+				'ie':'Ireland',
+				'il':'Israel',
+				'it':'Italy',
+				'jp':'Japan',
+				'kr':'Korea',
+				'mx':'Mexico',
+				'nl':'Netherlands',
+				'nz':'New Zealand',
+				'ni':'Nicaragua',
+				'pk':'Pakistan',
+				'pa':'Panama',
+				'pe':'Peru',
+				'pl':'Poland',
+				'pt':'Portugal',
+				'qa':'Qatar',
+				'ro':'Romania',
+				'ru':'Russia',
+				'sa':'Saudi Arabia',
+				'za':'South Africa',
+				'es':'Spain',
+				'ch':'Switzerland',
+				'sy':'Syria',
+				'tw':'Taiwan',
+				'th':'Thailand',
+				'tr':'Turkey',
+				'ua':'Ukraine',
+				'gb':'United Kingdom',
+				'us':'United States Of America',
+				'uy':'Uruguay',
+				've':'Venezuela'
+			}
